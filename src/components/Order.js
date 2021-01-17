9547558724167a4efcccc2f73d52027d51c59ac3
oliver <olivercastroteran@gmail.com>
2021-01-16 23:10:42 -0400

    modal added

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 const containerVariants = {
@@ -32,7 +32,13 @@ const childVariants = {
   },
 };
 
-const Order = ({ pizza }) => {
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 5000);
+  }, []);
+
   return (
     <motion.div
       className="container order"
